896ee3b2d815cf27cb234592d1bfeb8d077e3d74
avenable11 <48918177+avenable11@users.noreply.github.com>
2024-01-18 03:29:20 +0000

    final demo 01/17/2024

---
+++ 01_17_24/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main()
+{
+    int x = 5;
+    int y = 2;
+    double length = x + y;
+    // x = 57;
+    std::cout << x / y << std::endl;
+    std::cout << x % y << std::endl;
+    int bob = 7;
+
+    bob = 90;
+    std::cout << "Enter bob's age: ";
+    std::cin >> bob;
+
+    return 0;
+}
