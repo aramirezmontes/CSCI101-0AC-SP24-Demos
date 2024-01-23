732cbe5c0f5834d0246a006a4c2435b8efac6ec6
avenable11 <48918177+avenable11@users.noreply.github.com>
2024-01-23 03:06:33 +0000

    final demo 1/22/2024

--- 01_22_24/main.cpp
+++ 01_22_24/main.cpp
@@ -2,10 +2,78 @@
 #include <string>
 #include <cmath>
 #include <iomanip>
+#include <fstream>
 
 int main()
 {
-    std::string input;
+    std::ifstream order;
+    order.open("order.txt");
+    std::cout << std::left;
+    std::cout << "+-" << std::setw(33) << std::setfill('-') << "-"
+              << "+" << std::endl;
+    std::cout << std::setfill(' ');
+    std::cout << "| " << std::setw(15) << "Product"
+              << " | " << std::setw(15) << "Quantity"
+              << "|" << std::endl;
+    std::cout << "+-" << std::setw(33) << std::setfill('-') << "-"
+              << "+" << std::endl;
+    char product;
+    int quantity;
+    int totalQuantity = 0;
+    order >> product;
+    order >> quantity;
+    totalQuantity += quantity; // totalQuantity = totalQuantity + quantity;
+    std::cout << std::setfill(' ');
+    std::cout << "| " << std::setw(15) << product
+              << " | " << std::setw(15) << quantity
+              << "|" << std::endl;
+    std::cout << "+-" << std::setw(33) << std::setfill('-') << "-"
+              << "+" << std::endl;
+
+    order >> product;
+    order >> quantity;
+    totalQuantity += quantity;
+
+    std::cout << std::setfill(' ');
+    std::cout << "| " << std::setw(15) << product
+              << " | " << std::setw(15) << quantity
+              << "|" << std::endl;
+    std::cout << "+-" << std::setw(33) << std::setfill('-') << "-"
+              << "+" << std::endl;
+
+    order >> product;
+    order >> quantity;
+    totalQuantity += quantity;
+
+    std::cout << std::setfill(' ');
+    std::cout << "| " << std::setw(15) << product
+              << " | " << std::setw(15) << quantity
+              << "|" << std::endl;
+    std::cout << "+-" << std::setw(33) << std::setfill('-') << "-"
+              << "+" << std::endl;
+
+    order >> product;
+    order >> quantity;
+    totalQuantity += quantity;
+
+    std::cout << std::setfill(' ');
+    std::cout << "| " << std::setw(15) << product
+              << " | " << std::setw(15) << quantity
+              << "|" << std::endl;
+    std::cout << "+-" << std::setw(33) << std::setfill('-') << "-"
+              << "+" << std::endl;
+
+    std::ofstream outputFile;
+    outputFile.open("orderOutput.txt");
+
+    outputFile << std::setprecision(2) << std::fixed << std::showpoint;
+    outputFile << "Total quantity = " << totalQuantity << std::endl;
+    outputFile << "Average quantity = " << totalQuantity / 4.0 << std::endl;
+
+    order.close();
+    outputFile.close();
+
+    /* std::string input;
     double exp;
     std::cout << std::setprecision(2) << std::fixed << std::showpoint;
 
@@ -38,7 +106,7 @@ int main()
     double answer = std::pow(5, exp);
     std::cout << std::setprecision(3) << answer << " ";
     std::cout << std::setprecision(4) << answer << " ";
-    std::cout << std::endl;
+    std::cout << std::endl; */
 
     return 0;
 }
