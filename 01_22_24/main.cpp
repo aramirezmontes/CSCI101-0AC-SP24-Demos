a4e7d4691a9fdffb1634f4bacb29a64816b17b6e
avenable11 <48918177+avenable11@users.noreply.github.com>
2024-01-23 02:13:54 +0000

    setting up file i/o demo

--- 01_22_24/main.cpp
+++ 01_22_24/main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <iomanip>
 
 int main()
 {
     std::string input;
-    int exp;
+    double exp;
+    std::cout << std::setprecision(2) << std::fixed << std::showpoint;
 
     std::cout << "Enter the exponent: ";
     std::cin >> exp;
     std::cout << std::endl;
     // std::cin.clear();
     // std::cin >> exp;
-    int x;
-    std::cin >> x;
+    // int x;
+    // std::cin >> x;
     char ch;
-    std::cin.get(ch);
-    // remove leading whitespace from input stream
+    // std::cin.get(ch);
+    //  remove leading whitespace from input stream
 
     std::cout << "Enter something: ";
     std::cin >> std::ws;
@@ -33,7 +35,10 @@ int main()
     std::cout << std::endl;
     std::cout << "You entered " << input << std::endl;
 
-    double answer = std::pow(2, exp);
+    double answer = std::pow(5, exp);
+    std::cout << std::setprecision(3) << answer << " ";
+    std::cout << std::setprecision(4) << answer << " ";
+    std::cout << std::endl;
 
     return 0;
 }
