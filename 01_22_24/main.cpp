f707e6148e5f25cc4b25c8842555e15a90d91a94
avenable11 <48918177+avenable11@users.noreply.github.com>
2024-01-23 01:39:39 +0000

    up to second break 1/22

--- 01_22_24/main.cpp
+++ 01_22_24/main.cpp
@@ -10,18 +10,30 @@ int main()
     std::cout << "Enter the exponent: ";
     std::cin >> exp;
     std::cout << std::endl;
+    // std::cin.clear();
+    // std::cin >> exp;
+    int x;
+    std::cin >> x;
+    char ch;
+    std::cin.get(ch);
+    // remove leading whitespace from input stream
 
     std::cout << "Enter something: ";
+    std::cin >> std::ws;
     // std::cin >> input; only gets one word up to a space
     std::getline(std::cin, input);
     std::cout << std::endl;
 
     std::cout << "You entered " << input << std::endl;
 
-    double answer = std::pow(2, exp);
-
-    char ch;
+    std::cout << "Enter something else ";
     // std::cin.get(ch);
+    std::cin >> std::ws;
+    std::getline(std::cin, input);
+    std::cout << std::endl;
+    std::cout << "You entered " << input << std::endl;
+
+    double answer = std::pow(2, exp);
 
     return 0;
 }
