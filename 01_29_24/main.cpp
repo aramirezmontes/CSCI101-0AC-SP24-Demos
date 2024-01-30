8ee78859f0c90ce1d36bc6da7fbd8bcdbdefdb5a
avenable11 <48918177+avenable11@users.noreply.github.com>
2024-01-30 02:04:54 +0000

    nested if

--- 01_29_24/main.cpp
+++ 01_29_24/main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
 
 int main()
 {
     int magicNum = 87;
     int userNum;
+    std::string day;
 
     std::cout << "Enter a number: ";
     std::cin >> userNum;
     std::cout << std::endl;
+    std::cout << "Enter a day: ";
+    std::cin >> day;
+    std::cout << std::endl;
 
-    if (userNum <= magicNum)
+    std::transform(day.begin(), day.end(), day.begin(), ::tolower); // to transform to upper case replace ::tolower with ::toupper
+
+    if (userNum <= magicNum /* || day != "monday" */)
     {
+        if (day == "monday")
+            std::cout << "It is Monday" << std::endl;
         std::cout << "I won!!" << std::endl;
         std::cout << "I have the highest number because I'm the best!!!!" << std::endl;
     }
