d0ec84c1e3eecd67ade9153f85c4795fcbac0d98
aramirezmontes <156997563+aramirezmontes@users.noreply.github.com>
2024-01-29 22:39:08 -0500

    added the statments

--- 01_29_24/main.cpp
+++ 01_29_24/main.cpp
@@ -66,54 +66,69 @@ int main()
         break;
     case '1':
         std::cout << "0001" << std::endl;
+        binary = "0001";
         break;
     case '2':
         std::cout << "0010" << std::endl;
+        binary = "0010";
         break;
     case '3':
         std::cout << "0011" << std::endl;
+        binary = "0011";
         break;
     case '4':
         std::cout << "0100" << std::endl;
+          binary = "0100";
         break;
     case '5':
         std::cout << "0101" << std::endl;
+        binary = "0101";
         break;
     case '6':
         std::cout << "0110" << std::endl;
+         binary = "0110";
         break;
     case '7':
         std::cout << "0111" << std::endl;
+         binary = "0111";
         break;
     case '8':
         std::cout << "1000" << std::endl;
+        binary = "1000";
         break;
     case '9':
         std::cout << "1001" << std::endl;
+        binary = "1001";
         break;
     case 'A':
     case 'a':
         std::cout << "1010" << std::endl;
+        binary = "1010";
         break;
     case 'B':
     case 'b':
         std::cout << "1011" << std::endl;
+        binary = "1011";
         break;
     case 'C':
     case 'c':
         std::cout << "1100" << std::endl;
+        binary = "1100";
         break;
     case 'D':
     case 'd':
         std::cout << "1101" << std::endl;
+        binary = "1101";
         break;
     case 'E':
     case 'e':
         std::cout << "1110" << std::endl;
+        binary = "1110";
         break;
     case 'F':
     case 'f':
         std::cout << "1111" << std::endl;
+        binary = "1111";
         break;
     default:
         std::cout << "You did not enter a hex digit" << std::endl;
