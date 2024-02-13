4cf3def1dc5c14d307c617a75b658d75f95a5d00
Angela <avenable11@ivytech.edu>
2024-02-12 20:51:16 -0500

    more demo

--- 02_12_24/main.cpp
+++ 02_12_24/main.cpp
@@ -2,15 +2,32 @@
 #include <string>
 #include <limits>
 #include <cmath>
+#include <cctype>
+
+const std::string UNIT_PROMPT = "Enter the byte units (B for bytes, K for KB, M for MB, G for GB, T for TB, P for PB, or E for EB): ";
 
 int getIntGreaterThan0(std::string prompt);
+long calculateRawBytes(int memSize, char byteUnit);
+int secret(int x);
+char getCharacter(std::string prompt);
+bool isValidByteUnit(char byteUnit);
 
 int main()
 {
     int memSize;
     memSize = getIntGreaterThan0("Enter the memory size: ");
+    char unit = getCharacter(UNIT_PROMPT);
+    while (!isValidByteUnit(unit))
+    {
+        std::cout << "The byte unit entered is invalid." << std::endl;
+        unit = getCharacter(UNIT_PROMPT);
+    }
 
-    std::cout << "The minimum MAR size is " << ceil(log2(memSize)) << std::endl;
+    long rawmem = calculateRawBytes(memSize, unit);
+    int y = secret(2);
+    y = secret(7);
+
+    std::cout << "The minimum MAR size is " << ceil(log2(rawmem)) << std::endl;
 
     return 0;
 }
@@ -38,4 +55,66 @@ int getIntGreaterThan0(std::string prompt)
         std::cout << std::endl;
     }
     return number;
+}
+
+long calculateRawBytes(int memSize, char byteUnit)
+{
+    int exponent;
+
+    switch (byteUnit)
+    {
+    case 'k':
+    case 'K':
+        exponent = 10;
+        break;
+    case 'm':
+    case 'M':
+        exponent = 20;
+        break;
+    case 'g':
+    case 'G':
+        exponent = 30;
+        break;
+    case 't':
+    case 'T':
+        exponent = 40;
+        break;
+    case 'p':
+    case 'P':
+        exponent = 50;
+        break;
+    case 'e':
+    case 'E':
+        exponent = 60;
+        break;
+    default:
+        exponent = 0;
+    }
+
+    return memSize * pow(2, exponent);
+}
+
+int secret(int x)
+{
+    if (x > 5)
+        return 2 * x;
+    return x;
+}
+
+char getCharacter(std::string prompt)
+{
+    char x;
+    std::cout << prompt;
+    std::cin >> x;
+    std::cout << std::endl;
+    return x;
+}
+
+bool isValidByteUnit(char byteUnit)
+{
+    bool valid;
+    byteUnit = toupper(byteUnit);
+    valid = byteUnit == 'K' || byteUnit == 'M' || byteUnit == 'G' || byteUnit == 'T' ||
+            byteUnit == 'P' || byteUnit == 'E' || byteUnit == 'B';
+    return valid;
 }
