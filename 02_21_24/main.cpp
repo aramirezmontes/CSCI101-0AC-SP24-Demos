d62f6bb9ea1c1f24611aed919dc6767663f2996e
Angela Venable <avenable11@ivytech.edu>
2024-02-22 01:26:46 +0000

    lecture activity

--- 02_21_24/main.cpp
+++ 02_21_24/main.cpp
@@ -3,51 +3,51 @@
 #include <limits>
 
 const int LIST_SIZE = 10;
+const int NUM_COLORS = 6;
+
+enum colorType
+{
+    RED,
+    BLUE,
+    GREEN,
+    PURPLE,
+    ORANGE,
+    YELLOW
+};
+const std::string colorNames[NUM_COLORS] = {"Red", "Blue", "Green", "Purple", "Orange", "Yellow"};
+const colorType colors[NUM_COLORS] = {
+    RED,
+    BLUE,
+    GREEN,
+    PURPLE,
+    ORANGE,
+    YELLOW};
 
 void resetStream();
+void initialize(int list[], int listSize, int initialValue = 0);
+void inputArray(int list[], int listSize, std::string prompt);
+void outputArray(const int list[], int listSize);
+void calculateSum(const int list[], int listSize, double &sum);
+void findLargest(const int list[], int listSize, int &largestPos);
+void printColorMenu();
+// lecture activity rewrite the function below (from Monday) to reference the constant arrays
+colorType inputColor();
 
 int main()
 {
     int list[LIST_SIZE] = {5};
-    for (int i = 0; i < LIST_SIZE; i++)
-    {
-        list[i] = -99;
-    }
 
-    for (int i = 0; i < LIST_SIZE; i++)
-    {
-        std::cout << "Enter number #" << i + 1 << ": ";
-        std::cin >> list[i];
-        std::cout << std::endl;
-        while (!std::cin)
-        {
-            resetStream();
-            std::cout << "Enter number #" << i + 1 << ": ";
-            std::cin >> list[i];
-            std::cout << std::endl;
-        }
-    }
-
-    for (int i = 0; i < LIST_SIZE; i++)
-    {
-        std::cout << list[i] << std::endl;
-    }
+    initialize(list, LIST_SIZE);
+    inputArray(list, LIST_SIZE, "Enter a number: ");
+    outputArray(list, LIST_SIZE);
 
     double sum = 0;
-    for (int i = 0; i < LIST_SIZE; i++)
-    {
-        sum += list[i]; // sum = sum + list[i];
-    }
+    calculateSum(list, LIST_SIZE, sum);
+
     std::cout << "The sum is " << sum << " and the average is " << sum / LIST_SIZE << std::endl;
 
     int largestPos = 0;
-    for (int i = 1; i < LIST_SIZE; i++)
-    {
-        if (list[largestPos] < list[i])
-        {
-            largestPos = i;
-        }
-    }
+    findLargest(list, LIST_SIZE, largestPos);
 
     std::cout << "The largest element is " << list[largestPos] << " at position " << largestPos << std::endl;
 
@@ -59,4 +59,67 @@ void resetStream()
     std::cout << "You entered something that is not a number!" << std::endl;
     std::cin.clear();
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void initialize(int list[], int listSize, int initialValue)
+{
+    for (int i = 0; i < listSize; i++)
+    {
+        list[i] = initialValue;
+    }
+}
+
+void inputArray(int list[], int listSize, std::string prompt)
+{
+    for (int i = 0; i < listSize; i++)
+    {
+        std::cout << prompt;
+        std::cin >> list[i];
+        std::cout << std::endl;
+        while (!std::cin)
+        {
+            resetStream();
+            std::cout << prompt;
+            std::cin >> list[i];
+            std::cout << std::endl;
+        }
+    }
+}
+
+void outputArray(const int list[], int listSize)
+{
+    for (int i = 0; i < listSize; i++)
+    {
+        std::cout << list[i] << std::endl;
+    }
+}
+
+void calculateSum(const int list[], int listSize, double &sum)
+{
+    sum = 0;
+    for (int i = 0; i < listSize; i++)
+    {
+        sum += list[i]; // sum = sum + list[i];
+    }
+}
+
+void findLargest(const int list[], int listSize, int &largestPos)
+{
+    largestPos = 0;
+    for (int i = 1; i < listSize; i++)
+    {
+        if (list[largestPos] < list[i])
+        {
+            largestPos = i;
+        }
+    }
+}
+
+void printColorMenu()
+{
+    std::cout << "Please choose a color:" << std::endl;
+    for (int i = 0; i < NUM_COLORS; i++)
+    {
+        std::cout << i + 1 << ". " << colorNames[i] << std::endl;
+    }
 }
